0325043366d7d617e273ce27d624cc1a28ab6c05
xwaynex <nobodysshinigami@proton.me>
2025-03-04 18:45:07 +0000

    authOptions fix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,62 +1,6 @@
-import NextAuth, { Session } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { JWT } from "next-auth/jwt";
+import NextAuth from "next-auth";
 
-// Define auth options inside the route file
-const authOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-  callbacks: {
-    async session({ session, token }: { session: Session; token: JWT })  {
-      if (session.user && token.sub) {
-        return {
-          ...session,
-          user: {
-            ...session.user,
-            tag: session.user.name?.split(" ").join("").toLowerCase(),
-            uid: token.sub,
-          },
-        };
-      }
-      return session;
-    },
-  },
-  debug: true,
-  secret: process.env.NEXTAUTH_SECRET, // Required in production
-};
-
-
-// export const handler = NextAuth({
-//   providers: [
-//     GoogleProvider({
-//       clientId: process.env.GOOGLE_CLIENT_ID!,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-
-//     }),
-//   ],
-//   callbacks: {
-//     async session({ session, token }: { session: Session; token: JWT }) {
-//       if (session.user && token.sub) {
-//         return {
-//           ...session,
-//           user: {
-//             ...session.user,
-//             tag: session.user.name?.split(" ").join("").toLowerCase(),
-//             uid: token.sub,
-//           },
-//         };
-//       }
-//       return session;
-//     }
-    
-//   },
-//   debug: true,  // Enable debugging logs
-//   secret: process.env.NEXTAUTH_SECRET, // Required in production
-// });
+import authOptions from "@/lib/config/auth/auth";
 
 const handler = NextAuth(authOptions);
 
